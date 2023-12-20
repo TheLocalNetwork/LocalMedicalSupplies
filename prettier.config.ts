da9edf6afbdda59b18ca1544b73772b15c818e8f
Jason Rushton <jason@jasonrushton.com>
2023-12-19 20:30:26 -0700

    add sql formatting

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,9 +1,25 @@
-import { type Options } from 'prettier';
-
-const config: Options = {
+/** @type {import('prettier').Config} */
+const prettierConfig = {
+  plugins: ['prettier-plugin-embed', 'prettier-plugin-sql'],
   printWidth: 120,
   semi: true,
   trailingComma: 'es5',
 };
 
-export default config;
+/** @type {import('prettier-plugin-embed').PrettierPluginEmbedOptions} */
+const prettierPluginEmbedConfig = {
+  embeddedSqlIdentifiers: ['sql'],
+};
+
+/** @type {import('prettier-plugin-sql').SqlBaseOptions} */
+const prettierPluginSqlConfig = {
+  language: 'sqlite',
+  keywordCase: 'upper',
+  comma: 'before',
+};
+
+export default {
+  ...prettierConfig,
+  ...prettierPluginEmbedConfig,
+  ...prettierPluginSqlConfig,
+};
