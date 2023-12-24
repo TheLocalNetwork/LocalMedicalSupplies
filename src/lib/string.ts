4ef246f17060f0a1645ea63a2cbb747cb6cd45d7
Jason Rushton <jason@jasonrushton.com>
2023-12-24 15:47:09 -0700

    add string utilities

--- src/lib/string.ts
+++ src/lib/string.ts
@@ -1,5 +1,13 @@
-import { deburr, kebabCase } from 'lodash';
+import { deburr, kebabCase } from "lodash";
 
 export const slugify = (str: string): string => {
   return kebabCase(deburr(str));
+};
+
+export const booleanToYesNo = (value: boolean) => {
+  return value ? "Yes" : "No";
+};
+
+export const isoDateToLocaleDate = (value: string) => {
+  return value ? new Date(value).toLocaleDateString() : null;
 };
