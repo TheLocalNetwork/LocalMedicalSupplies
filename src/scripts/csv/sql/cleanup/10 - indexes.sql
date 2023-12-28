4e3b478d0e521933dd033f1ac9497e67b4b37a6b
Jason Rushton <jason@jasonrushton.com>
2023-12-28 12:42:17 -0700

    add IX_SUPPLIER_zip__practice_name

--- src/scripts/csv/sql/cleanup/10 - indexes.sql
+++ src/scripts/csv/sql/cleanup/10 - indexes.sql
@@ -1,3 +1,5 @@
 CREATE INDEX IX_SUPPLIER_SUPPLY_provider_id__supply_id ON SUPPLIER_SUPPLY (provider_id, supply_id);
 
 CREATE INDEX IX_SUPPLIER_zip ON SUPPLIER(zip);
+
+CREATE INDEX IX_SUPPLIER_zip__practice_name ON SUPPLIER(zip, practice_name);
