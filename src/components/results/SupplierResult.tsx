d20b309d86e97d2f0159022206da5076a68adb9f
Jason Rushton <jason@jasonrushton.com>
2023-12-29 20:25:51 -0700

    style the supplier results

--- src/components/results/SupplierResult.tsx
+++ src/components/results/SupplierResult.tsx
@@ -1,39 +1,103 @@
+import { ChevronRightIcon } from '@heroicons/react/24/outline';
+import { InformationCircleIcon } from '@heroicons/react/24/solid';
+import clsx from 'clsx';
 import Link from 'next/link';
+import { Badge } from '~/components/catalyst/badge';
 import { getBrowseLink } from '~/lib/link/browse';
 import { getSupplierLink } from '~/lib/link/supplier';
 import { type IGeoSupplierResults } from './lookupSuppliers';
-
 
 export interface ISupplierResultProps {
   supplier: IGeoSupplierResults;
 }
 export const SupplierResult: React.FC<ISupplierResultProps> = ({ supplier }) => {
   const supplierLink = getSupplierLink(supplier);
+
+  return (
+    <li key={supplier.id} className="flex  w-full max-w-full flex-row items-center gap-6 overflow-auto px-2 py-6 ">
+      <div className="hidden shrink lg:block">
+        <Link href={supplierLink}>
+          <InformationCircleIcon className="size-6 text-emerald-600 opacity-50 sm:size-16 dark:text-emerald-300" />
+        </Link>
+      </div>
+      <div className="flex w-full max-w-full flex-col justify-between gap-3 overflow-hidden ">
+        <Link
+          className="block overflow-hidden text-ellipsis whitespace-nowrap text-xl hover:underline"
+          href={supplierLink}
+        >
+          {supplier.practice_name}
+        </Link>
+
+        <SupplierAddress supplier={supplier} className="" />
+        <SupplierBadges supplier={supplier} className="" />
+      </div>
+      <div className="hidden shrink sm:block">
+        <Link className="hover:underline" href={supplierLink}>
+          <ChevronRightIcon className="size-8 opacity-50 hover:opacity-100" />
+        </Link>
+      </div>
+    </li>
+  );
+};
+
+interface ISupplierAddressProps extends React.ComponentPropsWithoutRef<'div'> {
+  supplier: IGeoSupplierResults;
+}
+const SupplierAddress: React.FC<ISupplierAddressProps> = ({ supplier, className, ...attrs }) => {
+  const supplierLink = getSupplierLink(supplier);
   const stateLink = getBrowseLink({ state: supplier.StateSlug });
   const cityLink = getBrowseLink({ state: supplier.StateSlug, city: supplier.CitySlug });
   const zipLink = getBrowseLink({ zip: supplier.zip });
 
   return (
-    <li key={supplier.id} className="p-4">
-      <div className="text-xl">
-        <Link href={supplierLink}>{supplier.practice_name}</Link>
-      </div>
-
-      <div className="py-1 text-sm">
-        {supplier.practice_slug !== supplier.business_slug ? (
-          <div>
-            <Link href={supplierLink}>{supplier.business_name}</Link>
-          </div>
-        ) : null}
-
+    <div {...attrs} className={clsx('flex flex-col gap-2 px-2 text-xs italic leading-none opacity-70', className)}>
+      {supplier.practice_slug !== supplier.business_slug ? (
         <div>
-          <Link href={cityLink}>{supplier.CityName}</Link>
-          {', '}
-          <Link href={stateLink}>{supplier.StateAbbr}</Link>
-          {` `}
-          <Link href={zipLink}>{supplier.zip}</Link>
+          <Link className="font-bold hover:underline" href={supplierLink}>
+            {supplier.business_name}
+          </Link>
         </div>
+      ) : null}
+      <div>
+        <Link className="hover:underline" href={cityLink}>
+          {supplier.CityName}
+        </Link>
+        {', '}
+        <Link className="hover:underline" href={stateLink}>
+          {supplier.StateAbbr}
+        </Link>
+        {` `}
+        <Link className="hover:underline" href={zipLink}>
+          {supplier.zip}
+        </Link>
       </div>
-    </li>
+    </div>
+  );
+};
+interface ISupplierBadgesProps extends React.ComponentPropsWithoutRef<'div'> {
+  supplier: IGeoSupplierResults;
+}
+const SupplierBadges: React.FC<ISupplierBadgesProps> = ({ supplier, className, ...attrs }) => {
+  return (
+    <div {...attrs} className={clsx('flex gap-2', className)}>
+      <Badge
+        color={supplier.accepts_assignment ? 'green' : 'red'}
+        className={clsx({
+          'opacity-50': !supplier.accepts_assignment,
+        })}
+      >
+        <span className="hidden sm:block">{`Accepts Medicare Assignment`}</span>
+        <abbr className="block cursor-help sm:hidden" title={`Accepts Assignment`}>{`Assignment`}</abbr>
+      </Badge>
+      <Badge
+        color={supplier.is_contracted_for_cba ? 'green' : 'red'}
+        className={clsx({
+          'opacity-50': !supplier.is_contracted_for_cba,
+        })}
+      >
+        <span className="hidden sm:block">{`Competitive Bidding Area`}</span>
+        <abbr className="block cursor-help sm:hidden " title={`Competitive Bidding Area`}>{`CBA`}</abbr>
+      </Badge>
+    </div>
   );
 };
