35d97ec921adf21353d7f9130fb49b507bc77910
Jason Rushton <jason@jasonrushton.com>
2023-12-29 22:31:54 -0700

    add urlSearchParams debug output

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import { isEmpty, isNil } from 'lodash';
 import { type Metadata } from 'next';
-import { Results } from '~/components/results/results';
 import { Card } from '~/components/elements/Card';
 import { Form } from '~/components/form/form';
+import { Results } from '~/components/results/results';
 import { CANONICAL_SITE_NAME } from '~/lib/const';
 import { type IFilterParams } from '~/types/filters';
 
@@ -13,6 +13,9 @@ interface IProps {
 export default function HomePage(props: IProps) {
   const { searchParams } = props;
   const urlSearchParams = new URLSearchParams(searchParams);
+
+  // eslint-disable-next-line no-console
+  console.info({ urlSearchParams });
 
   for (const [key, value] of urlSearchParams.entries()) {
     if (isNil(value) || isEmpty(value)) urlSearchParams.delete(key);
@@ -26,9 +29,9 @@ export default function HomePage(props: IProps) {
   return (
     <article className={'flex flex-col gap-12'}>
       <Card>
-        <div key={urlSearchParams.toString()} className="flex flex-col md:flex-row items-start gap-8">
-          <Form urlSearchParams={urlSearchParams} />
-          <Results filterParams={filterParams} />
+        <div key={urlSearchParams.toString()} className="flex flex-col items-start gap-8 md:flex-row">
+          <Form key={urlSearchParams.toString()} urlSearchParams={urlSearchParams} />
+          <Results key={urlSearchParams.toString()} filterParams={filterParams} />
         </div>
       </Card>
     </article>
