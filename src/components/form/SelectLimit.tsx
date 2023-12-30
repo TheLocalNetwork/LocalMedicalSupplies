f3376ba69fc47db9f80f83eeb0c930de903da780
Jason Rushton <jason@jasonrushton.com>
2023-12-29 22:20:56 -0700

    use param hook

--- src/components/form/SelectLimit.tsx
+++ src/components/form/SelectLimit.tsx
@@ -1,9 +1,8 @@
 'use client';
-import { isNil } from 'lodash';
-import { useRouter } from 'next/navigation';
 import { Field, Label } from '~/components/catalyst/fieldset';
 import { Listbox, ListboxLabel, ListboxOption } from '~/components/catalyst/listbox';
-import { DEFAULT_LIMIT, MAX_LIMIT } from '~/components/form/consts';
+import { DEFAULT_LIMIT } from '~/components/form/consts';
+import { useSetLimit } from '~/components/form/urlParamsRouting';
 
 const selectLimitOptions = [10, 20, 50];
 export interface ISelectLimitProps {
@@ -13,35 +12,13 @@ export const SelectLimit: React.FC<ISelectLimitProps> = ({ urlSearchString }) =>
   const urlSearchParams = new URLSearchParams(urlSearchString);
   const urlValue = urlSearchParams.get('limit');
   const defaultValue = urlValue ? urlValue : DEFAULT_LIMIT.toString();
-  const router = useRouter();
-
-  const handleChange = (proposedLimit: string | null) => {
-    if (isNil(proposedLimit)) return;
-
-    const numLimit = parseInt(proposedLimit, 10);
-
-    if (numLimit > MAX_LIMIT) proposedLimit = `${MAX_LIMIT}`;
-    if (numLimit < 10) proposedLimit = `10`;
-
-    const proposedSearchParams = new URLSearchParams(urlSearchParams);
-
-    if (numLimit === DEFAULT_LIMIT) {
-      proposedSearchParams.delete('limit');
-    } else {
-      proposedSearchParams.set('limit', proposedLimit);
-    }
-
-    proposedSearchParams.delete('offset');
-
-    const toUrl = [`/`, proposedSearchParams.toString()].join(`?`);
-    return router.push(toUrl);
-  };
+  const setLimit = useSetLimit(urlSearchParams);
 
   return (
     <>
       <Field>
         <Label>Page Size</Label>
-        <Listbox name="limit" defaultValue={defaultValue} onChange={handleChange}>
+        <Listbox name="limit" defaultValue={defaultValue} onChange={setLimit}>
           {selectLimitOptions.map((limit) => (
             <ListboxOption key={limit} value={limit.toString()}>
               <ListboxLabel>{limit}</ListboxLabel>
