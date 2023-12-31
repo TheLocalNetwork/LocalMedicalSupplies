ec52995562446814c74301d8d631b5241de9a9a1
Jason Rushton <jason@jasonrushton.com>
2023-12-31 16:01:16 -0700

    add indexes for product and manufacturer

--- src/scripts/csv/sql/cleanup/10 - indexes.sql
+++ src/scripts/csv/sql/cleanup/10 - indexes.sql
@@ -1,3 +1,7 @@
+CREATE INDEX IX_PRODUCT_slug__manufacturer_id ON PRODUCT (slug, manufacturer_id);
+
+CREATE INDEX IX_SUPPLIER_PRODUCT_product_id ON SUPPLIER_PRODUCT (product_id);
+
 CREATE INDEX IX_SUPPLIER_SUPPLY_provider_id__supply_id ON SUPPLIER_SUPPLY (provider_id, supply_id);
 
 CREATE INDEX IX_SUPPLIER_zip ON SUPPLIER (zip);
