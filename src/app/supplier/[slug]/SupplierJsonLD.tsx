0824e33e66e99e3ee7b97a06bbc8a98bc760bf41
Jason Rushton <jason@jasonrushton.com>
2024-01-01 19:47:04 -0700

    testing jsonld

--- src/app/supplier/[slug]/SupplierJsonLD.tsx
+++ src/app/supplier/[slug]/SupplierJsonLD.tsx
@@ -5,6 +5,8 @@ interface IJsonLDProps {
   supplier: IGeoSupplier;
 }
 export const SupplierJsonLD: React.FC<IJsonLDProps> = ({ supplier }) => {
+  // const offerCatalog = getOfferCatalog(supplier);
+
   const jsonLd = {
     '@context': 'https://schema.org',
     '@type': 'MedicalBusiness',
@@ -21,6 +23,83 @@ export const SupplierJsonLD: React.FC<IJsonLDProps> = ({ supplier }) => {
       'postalCode': supplier.zip,
       'addressCountry': 'US',
     },
+    // 'hasOfferCatalog': offerCatalog,
   };
   return <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />;
 };
+
+// export const getOfferCatalog = (supplier: ISupplier) => {
+//   const categories = getAllSupplierSupply({ provider_id: supplier.id });
+//   const categoryOffers = {
+//     '@type': 'OfferCatalog',
+//     'name': 'Equipment Category',
+//     'itemListElement': map(categories, (item) => {
+//       const url = getParamsUrl(new ImmutableURLSearchParams({ equipment: item.slug }), true);
+//       return {
+//         '@type': 'Offer',
+//         'itemOffered': {
+//           '@type': 'Equipment Category',
+//           '@id': url,
+//           'name': item.name,
+//         },
+//       };
+//     }),
+//   };
+
+//   const specialities = getAllSupplierSpeciality({ provider_id: supplier.id });
+//   const specialityOffers = {
+//     '@type': 'OfferCatalog',
+//     'name': 'Speciality',
+//     'itemListElement': map(specialities, (item) => {
+//       const url = getParamsUrl(new ImmutableURLSearchParams({ speciality: item.slug }), true);
+//       return {
+//         '@type': 'Offer',
+//         'itemOffered': {
+//           '@type': 'Speciality',
+//           '@id': url,
+//           'name': item.name,
+//         },
+//       };
+//     }),
+//   };
+
+//   const providertypes = getAllSupplierProviderType({ provider_id: supplier.id });
+//   const providertypesOffers = {
+//     '@type': 'OfferCatalog',
+//     'name': 'Provider Type',
+//     'itemListElement': map(providertypes, (item) => {
+//       const url = getParamsUrl(new ImmutableURLSearchParams({ providertype: item.slug }), true);
+//       return {
+//         '@type': 'Offer',
+//         'itemOffered': {
+//           '@type': 'Provider Type',
+//           '@id': url,
+//           'name': item.name,
+//         },
+//       };
+//     }),
+//   };
+
+//   const products = getAllSupplierProduct({ provider_id: supplier.id });
+//   const productOffers = {
+//     '@type': 'OfferCatalog',
+//     'name': 'Product',
+//     'itemListElement': map(products, (product) => {
+//       const url = getParamsUrl(
+//         new ImmutableURLSearchParams({ product: product.slug, manufacturer: product.manufacturer_slug }),
+//         true
+//       );
+//       return {
+//         '@type': 'Offer',
+//         'itemOffered': {
+//           '@type': 'Product',
+//           '@id': url,
+//           'name': product.name,
+//           'manufacturer': product.manufacturer_name,
+//         },
+//       };
+//     }),
+//   };
+
+//   return [categoryOffers, specialityOffers, providertypesOffers, productOffers];
+// };
