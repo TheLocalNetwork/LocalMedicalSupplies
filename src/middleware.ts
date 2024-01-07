7951f8cee271329d0633ffe2fc5d0477d3ea2dcd
Jason Rushton <jason@jasonrushton.com>
2024-01-06 20:58:19 -0700

    tweak log output

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,11 @@ import { NextResponse, type NextRequest } from 'next/server';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const { method, nextUrl } = request;
-  const { hostname, pathname, searchParams } = nextUrl;
+  const { nextUrl } = request;
+  const { pathname, searchParams } = nextUrl;
   const ua = request.headers.get('user-agent');
 
-  const toLog = [method, hostname, pathname, searchParams, ua];
-
-  console.log(toLog.join(`\t`));
+  console.log([pathname, searchParams, ua].join(`\t`));
 
   return NextResponse.next();
 }
